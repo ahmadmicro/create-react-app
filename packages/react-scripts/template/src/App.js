29ddc72cb8159059920c4d4efabdf7d1e8880404
Ahmad Sadiq <sadiq.a.ahmad@gmail.com>
2018-06-02 13:13:09 +0100

    cummulative changes

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { conf } from './conf.js';
+import Layout from 'nodewire';
+import dashboard from './dashboard.html';
+import { data } from 'nodewire';
+
+console.log(data);
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <Layout layout={dashboard} format="xml" conf={conf} />;
   }
 }
 
---
+++ packages/react-scripts/template/src/conf.js
@@ -0,0 +1,9 @@
+export let conf = {
+  username: 'sadiq.a.ahmad@gmail.com',
+  password: 'secret',
+  instance: 'lrsnr49yxurz',
+  server: 'dashboard.nodewire.org',
+  trust_zones: [],
+  Nodes: [],
+  users: ['sadiq.a.ahmad@gmail.com'],
+};
