781664e7deba64df434be24c969294fd75e503bc
Ahmad Sadiq <sadiq.a.ahmad@gmail.com>
2018-09-01 00:45:34 +0100

    some

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 import { conf } from './conf.js';
 import Layout from 'nodewire';
 import dashboard from './dashboard.html';
-import { data } from 'nodewire';
-
-console.log(data);
 
 class App extends Component {
   render() {
